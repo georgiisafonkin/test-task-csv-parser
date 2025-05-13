b1b06d09498c9c13b25790b61fd8b20c7982e04a
Georgy <georgiisafonk@gmail.com>
2025-05-13 17:15:52 +0700

    employee модель добавлена

---
+++ src/models/employee.py
@@ -0,0 +1,16 @@
+from models.department import Department
+
+class Employee:
+    def __init__(self, employee_id: str, email: str, name: str, department: Department, hourly_rate: float, hours_worked: float) -> None:
+        self.employee_id = employee_id
+        self.email = email
+        self.name = name
+        self.department = department
+        self.hourly_rate = hourly_rate
+        self.hours_worked = hours_worked
+
+        department.add_employee(self)
+
+    @property
+    def payout(self) -> float:
+        return self.hourly_rate * self.hours_worked

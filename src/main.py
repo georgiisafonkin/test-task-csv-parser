4a292864b7868ccfd4b9a7ecd241686adc155ae6
Georgy <georgiisafonk@gmail.com>
2025-05-13 15:18:12 +0700

    Добавлен парсер командной строки

--- src/main.py
+++ src/main.py
@@ -3,6 +3,12 @@ import argparse
 cmd_line_parser = argparse.ArgumentParser(
     description=(
         "Генерация отчётов о сотрудниках по заданным CSV-файлам.\n"
-        "Использование: python main.py <файл1.csv> <файл2.csv> ... --report <тип_отчёта>"
     )
 )
+
+cmd_line_parser.add_argument("files", nargs="+", help=".csv файлы для генерации отчёта")
+cmd_line_parser.add_argument("--report", required=True, help="Тип отчёта (например, payout)")
+
+args = cmd_line_parser.parse_args()
+
+print(args)
